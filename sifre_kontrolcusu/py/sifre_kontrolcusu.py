e75c4492ad44f632c483b24ed690d40f6908d315
Mehmet Ali Şipi <mehmetalisipi@gmail.com>
2024-11-05 00:52:59 +0300

    Şifre Kontrolcüsünün Python sürümünü yayınla

---
+++ sifre_kontrolcusu/py/sifre_kontrolcusu.py
@@ -0,0 +1,17 @@
+def şifre_sızdırılmış_mı(dosya_konumu:str, şifre:str) -> bool:
+    dosya = open(dosya_konumu, "r");
+    yeni_satır:str = dosya.readline();
+    while yeni_satır != "":
+        yeni_satır = yeni_satır.split("\n")[0]; # Yeni satır karakterinden kurtulma vakti
+        if(yeni_satır == şifre):
+            return True;
+        yeni_satır = dosya.readline();
+    return False;
+
+if(__name__ == "__main__"):
+    dosya_konumu:str = input("Dosya Konumu: ");
+    şifre:str = input("Kontrol Edilmesi İstenen Şifre: ");
+    if(şifre_sızdırılmış_mı(dosya_konumu, şifre)):
+        print("Şifre Sızdırılmış");
+    else:
+        print("Güvendesiniz, Henüz :)");
